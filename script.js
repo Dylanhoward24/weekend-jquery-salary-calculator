6af29ae61c16e90905206e64875672f175872782
Dylan Howard <dylanhoward0424@outlook.com>
2021-07-26 18:50:19 -0500

    got really confused on the next step and went back to solve it a different way with arrays and objects

--- script.js
+++ script.js
@@ -9,53 +9,27 @@ function onReady() {
     
     $('#submitButton').on('click', addToList);
 
-    // declaration of monthly and annual variables
-    // and division function after addToList code
-    $('#totalMonthly').append(`${totalMonthlyCosts.toFixed(2)}`);
-
 }
 
-    // make sure the math works (it does)
-    // let totalAnnualCosts = 144;
-    // let totalMonthlyCosts = `${totalAnnualCosts}`/12;
-    let totalAnnualCosts = 0;
-    let totalMonthlyCosts = `${totalAnnualCosts}`/12;
-    
+let employees = [];
+
 // addToList function (this is what happens
 // whenever the submit button is clicked)
 function addToList() {
     console.log('added items to list');
 
-    // assign the first name value to a variable
-    let firstNameValue = $('#firstNameInputText').val();
-    console.log('firstNameValue', firstNameValue);
-    // assign the last name value to a variable
-    let lastNameValue = $('#lastNameInputText').val();
-    console.log('lastNameValue', lastNameValue);
-    // assign the ID value to a variable
-    let idValue = $('#idInputText').val();
-    console.log('idValue',idValue);
-    // assign the title value to a variable
-    let titleValue = $('#titleInputText').val();
-    console.log('titleValue', titleValue);
-    // assign the annual salary value to a variable
-    let annualSalaryValue = $('#annualSalaryInputText').val();
-    console.log('annualSalaryValue', annualSalaryValue);
-
-    $('#tableOfEmployees').append(`
-        <tr>
-            <th>${firstNameValue}</th>
-            <th>${lastNameValue}</th>
-            <th>${idValue}</th>
-            <th>${titleValue}</th>
-            <th>$${annualSalaryValue}</th>
-                <th>
-                    <button class="deleteBtn">Delete</button>
-                </th>
-        </tr>
-    `)
-
-    totalAnnualCosts+=annualSalaryValue;
+    let employee = {
+        firstName: $('#firstNameInputText').val(),
+        lastName: $('#lastNameInputText').val(),
+        id: $('#idInputText').val(),
+        title: $('#titleInputText').val(),
+        annualSalary: $('#annualSalaryInputText').val()
+    };
+    
+    console.log(employee);
+    
+    // push new object to employees array
+    employees.push(employee);
 
     // empty the input fields
     $('#firstNameInputText').val('');
